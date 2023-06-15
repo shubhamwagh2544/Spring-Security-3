709eed299c619c6b386d11cfa22e23c016731065
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-06-15 13:43:42 +0530

    creating user

---
+++ src/main/java/com/springboot/security3/user/Role.java
@@ -0,0 +1,6 @@
+package com.springboot.security3.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/springboot/security3/user/User.java
@@ -0,0 +1,73 @@
+package com.springboot.security3.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "id_seq"
+    )
+    @SequenceGenerator(
+            sequenceName = "id_seq",
+            name = "id_seq",
+            initialValue = 1,
+            allocationSize = 1
+    )
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
